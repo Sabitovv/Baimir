63e22f887f597fbbc39668962a5815c0f0b68982
Nurgalym Sabit <nurasabitov736@gmail.com>
2026-02-19 14:13:17 +0500

    new

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import './i18n'
+import './i18n/index.ts'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from '@/app/store'
