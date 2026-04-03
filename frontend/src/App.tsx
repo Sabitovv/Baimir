2af53500365f406c46fc48b44d72f32a540a0afb
Rus1IT <ruslankoldasov1@gmail.com>
2026-04-03 14:58:53 +0500

    werwer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
-import { type ReactNode } from 'react'
 import Layout from '@/components/layout/Layout'
 import AppRoutes from '@/routes/AppRoutes'
 
-// Вся сложная логика загрузки Tolgee теперь происходит ДО запуска React в index.tsx
 const App = () => {
   return (
     <Layout>
