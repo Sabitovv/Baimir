f5727cf2aaf8866099f1aabde69a1a9a32b7ce8a
Rus1IT <ruslankoldasov1@gmail.com>
2026-04-03 14:35:40 +0500

    newww

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,32 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import './i18n/index.ts'
+
+// Обязательно импортируем функции из твоего файла i18n
+import { initializeTolgee, isEditMode } from './i18n/index.ts' 
+
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from '@/app/store'
 
+// Оборачиваем запуск приложения в асинхронную функцию
+async function bootstrap() {
+  // Если мы в режиме перевода (перешли из админки по ссылке с параметрами),
+  // заставляем React подождать, пока Tolgee скачает UI-редактор и переводы.
+  if (isEditMode) {
+    await initializeTolgee();
+  }
 
+  // Только после загрузки Tolgee (или сразу, если мы в обычном режиме) 
+  // отрисовываем интерфейс.
+  createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
 
-createRoot(document.getElementById('root')!).render(
-<Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-</Provider>
-
-)
+// Запускаем инициализацию
+bootstrap();   
